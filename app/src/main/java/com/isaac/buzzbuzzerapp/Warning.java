c2f427da2591e964b29e1ec799d15b0c48bdb303
Parttimereaper <hoffmannisaac@gmail.com>
2016-04-24 13:50:18 -0500

    made theme

--- app/src/main/java/com/isaac/buzzbuzzerapp/Warning.java
+++ app/src/main/java/com/isaac/buzzbuzzerapp/Warning.java
@@ -8,6 +8,6 @@ public class Warning extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_warning);
     }
 }
