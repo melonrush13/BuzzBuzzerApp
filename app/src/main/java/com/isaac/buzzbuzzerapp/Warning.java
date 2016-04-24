3f1f49f8f9d4cf29165afac2544995a90ba90d7f
Parttimereaper <hoffmannisaac@gmail.com>
2016-04-24 15:35:11 -0500

    Added content and logic to signup page

--- app/src/main/java/com/isaac/buzzbuzzerapp/Warning.java
+++ app/src/main/java/com/isaac/buzzbuzzerapp/Warning.java
@@ -6,18 +6,15 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class Warning extends AppCompatActivity {
 
     private SharedPreferences preferences;
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warning);
-        button = (Button) findViewById(R.id.button);
         preferences = getPreferences(Context.MODE_PRIVATE);
         if(preferences.getBoolean("acceptedTerms", false)){
             if(preferences.getBoolean("hasAccount", false)){
@@ -33,6 +30,7 @@ public class Warning extends AppCompatActivity {
 
     public void onPress(View view){
         preferences.edit().putBoolean("acceptedTerms", true);
+        preferences.edit().apply();
         Intent intent = new Intent(this, SignUp.class);
         startActivity(intent);
     }
