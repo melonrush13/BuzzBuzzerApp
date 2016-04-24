39a3de55ea4351dd419981a00a0c81a2cc456f59
Parttimereaper <hoffmannisaac@gmail.com>
2016-04-24 13:36:54 -0500

    Added in all the activities, need to add functionality
    In addition labeled all the views in the draw.io file to reference these activities by name

---
+++ app/src/main/java/com/isaac/buzzbuzzerapp/Joining.java
@@ -0,0 +1,13 @@
+package com.isaac.buzzbuzzerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Joining extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joining);
+    }
+}
